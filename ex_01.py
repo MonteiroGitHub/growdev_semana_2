53c57747e2069f36545a57a318fea067f8e12094
João Pedro Monteiro <91032595+MonteiroGitHub@users.noreply.github.com>
2023-03-31 21:33:43 -0300

    Update ex_01.py

--- ex_01.py
+++ ex_01.py
@@ -3,11 +3,11 @@
 #O programa deve solicitar ao usuário que insira uma temperatura em graus Celsius e, em seguida, exiba a temperatura convertida em Fahrenheit. Após construir esse programa, modifique-o para que converta graus Fahrenheit em graus Celsius.
 
 #Versão de Celcius para Fahrenheit
-temp = int(input('Digite a temperatura em Celcius: '))
+temp = float(input('Digite a temperatura em Celcius: '))
 
 nova_temp = (9/5*temp) + 32
 
-print(f'{temp}°C em Fahrenheit é {nova_temp}°F ')
+print(f'{temp: .1f}°C em Fahrenheit é {nova_temp: .1f}°F ')
 
 #Versão de Fahrenheit para Celcius
 
@@ -15,4 +15,4 @@ temp = float(input('Digite a temperatura em Fahrenheit: '))
 
 nova_temp = (temp - 32) / 1.8
 
-print(f'{temp}°F em Celcius é {nova_temp}°C')
+print(f'{temp: .1f}°F em Celcius é {nova_temp: .1f}°C')
